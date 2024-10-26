147ceb9d6951ccb6422fc3cf0756fc659177b492
suyogbhalekar19 <146326726+suyogbhalekar19@users.noreply.github.com>
2024-10-26 23:21:37 +0530

    Create main.tf

---
+++ EKS_Cluster/main.tf
@@ -0,0 +1,30 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.26.0"
+  cluster_name    = locals.cluster_name
+  cluster_version = var.kubernetes_version
+  subnet_ids      = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  tags = {
+    cluster = "demo"
+  }
+
+  vpc_id = module.vpc.vpc_id
+
+  eks_managed_node_group_defaults = {
+    ami_type               = var.ami_value
+    instance_types         = var.instance_types_value
+    vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+  }
+
+  eks_managed_node_groups = {
+
+    node_group = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+  }
+}
